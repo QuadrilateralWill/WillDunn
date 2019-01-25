fea86720c7e520d454b840ac5a76e8222f0fb300
Will Dunn <dunn_powell@yahoo.com>
2019-01-25 14:11:05 -0700

    moved things

--- assignment3/FilterFunctions.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Will Dunn on 1/15/19.
-//
-#include <vector>
-#pragma once
-
-int parallelogramFilter(const std::vector<double> &points);
---
+++ code.sh
@@ -0,0 +1,8 @@
+#find the path the file that is testByLines.txt
+./quadrilateralClassifier testCasesByLine.txt
+
+#use the results to compare a difference between an accurrate answers file
+
+
+
+#if there are any differences, output that there are errors
