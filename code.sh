d48dafdf70384661693fbb14271d3bb746d385d8
Will Dunn <dunn_powell@yahoo.com>
2019-01-25 19:01:40 -0700

    altered var file to clear file before running bash script which was causing file to be appended to but never started from scratch

--- code.sh
+++ code.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#start by ensuring the var.txt is clear, so you are not just appending to past results
+truncate -s 0 var.txt
+
 #find the path the file that is testByLines.txt
 ./quadrilateralClassifier testCasesByLine.txt >> var.txt 
 
