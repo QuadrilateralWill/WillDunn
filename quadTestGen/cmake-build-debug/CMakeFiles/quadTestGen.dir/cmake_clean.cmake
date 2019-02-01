b00c8f3ca0ccb5d6e8a2ada862573514764d7130
Will Dunn <dunn_powell@yahoo.com>
2019-01-31 19:44:30 -0700

    created the file that will be making all the txt files for testing

---
+++ quadTestGen/cmake-build-debug/CMakeFiles/quadTestGen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadTestGen.dir/main.cpp.o"
+  "quadTestGen.pdb"
+  "quadTestGen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadTestGen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ quadTestGen/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main() {
+    const int NUMBER_OF_TESTS_TO_GENERATE = 1000;
+
+    //make files of different shapes
+
+    
+    //make null space files
+
+
+
+
+    /* legit quadrilateral time */
+    //make regular quadrilaterals
+
+
+    //make parallelograms
+
+
+    //make rectangles
+
+
+    //make rhombi
+
+
+    //make squares
+
+
+    //make trapezoids
+
+
+    //make kites
+
+
+    return 0;
+}
