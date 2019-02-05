5999deb6869ae08d338b29e6595065fc0b277983
Will Dunn <dunn_powell@yahoo.com>
2019-02-04 20:10:30 -0700

    python script now compares all files but needs aid in capturing output for error output

--- fuzz/fuzz.py
+++ fuzz/fuzz.py
@@ -16,6 +16,10 @@ os.chdir('/Users/willdunn/WillDunn') #go back to the higher folder to build the 
 
 
 def generateTestCoverage(optimizationNum):
+    failedTests = "";
+    correctResults = {'NullSpace':'Error 1', 'ASCII':'Error 1', 'Scalene':'quadrilateral','Quadrilateral':'quadrilateral',
+                      'Square':'square', 'Rhombus':'rhombus','Rectangle':'rectangle', 'Parallelogram':'parallelogram',
+                      'Trapezoid':'trapezoid','Kite':'kite',}
     os.system(
         'clang++ -fprofile-instr-generate -fcoverage-mapping main.cpp ParallelAndCongruentFunctions.cpp Errors.cpp '
         'FilterFunctions.cpp -O' + optimizationNum + ' -o quadrilateralClassifier')
@@ -28,19 +32,41 @@ def generateTestCoverage(optimizationNum):
         filename = os.path.splitext(os.fsdecode(file))[0]
         os.system(
             "LLVM_PROFILE_FILE=\"./coverage/" + filename + ".profraw\" ./quadrilateralClassifier quadTestGen/tests/" + filename + ".txt")
+        #result = os.system('echo')
         os.system(
             "xcrun llvm-profdata merge -sparse ./coverage/" + lastFile + ".profdata ./coverage/" + filename + ".profraw -o ./coverage/" + filename + ".profdata")
         lastFile = filename
-    os.system(
+        os.system(
         "xcrun llvm-cov show ./quadrilateralClassifier -instr-profile=./coverage/" + lastFile + ".profdata > coverage" +
         optimizationNum + ".txt")
+        #see if the test came out the way it should have using a dictionary
+        # nuFilename =''.join(i for i in filename if not i.isdigit()) #removes digits from test name
+        # if (nuFilename == '.DS_Store'): continue
+        # if(nuFilename != 'NullSpace' or nuFilename != 'ASCII'): #normal shape
+        #     if(correctResults[nuFilename] != result):
+        #         failedTests += filename + " "
+        # elif(nuFilename == 'NullSpace'):
+        #     if(correctResults[nuFilename] == result):
+        #         failedTests += filename + " "
+        # elif(nuFilename == 'ASCII'):
+        #     if(correctResults[nuFilename] != result):
+        #         failedTests += filename + " "
+    # if a test didn't arrive at the correct result, print 'ERROR' at the base of the program.  Else, print 'OK'
+    if(failedTests == ""):
+        with open("coverage" + optimizationNum + ".txt", "a+") as myfile:
+            myfile.write("OK")
+        myfile.close()
+    elif (failedTests != ""):
+        with open("coverage" + optimizationNum + ".txt", "a+") as myfile:
+            myfile.write("ERROR: " + failedTests)
+        myfile.close()
 
 
 #run in 3 different optimizations and ensure the same answer is returned
 generateTestCoverage('1')
 generateTestCoverage('2')
 generateTestCoverage('3')
-os.system('cmp --silent coverage1.txt coverage2.txt && echo "OK" || echo "Coverage 1 and 2 differ!\n"')
-os.system('cmp --silent coverage1.txt coverage3.txt && echo "OK" || echo "Coverage 1 and 3 differ!\n"')
+os.system('cmp --silent coverage1.txt coverage2.txt && echo "Optimization is the same" || echo "Coverage 1 and 2 differ!\n"')
+os.system('cmp --silent coverage1.txt coverage3.txt && echo "Optimization is the same" || echo "Coverage 1 and 3 differ!\n"')
 
 
